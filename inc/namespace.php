19d8aa7ba6f708fb88ef95926b8f52768da9a466
Chris Reynolds <chris@humanmade.com>
2019-09-02 14:21:45 -0600

    add base plugin files

---
+++ inc/namespace.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * HM Juicer
+ *
+ * Main plugin namespace for HM Juicer plugin.
+ *
+ * @package HM/Juicer
+ */
+
+namespace HM\Juicer;
+
+/**
+ * Kick everything off.
+ */
+function bootstrap() {}
---
+++ plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: HM Juicer
+ * Description: Integrates with Juicer API for social feeds and allows accessible display of social media content.
+ * Version: 0.1.0
+ * Author: Human Made
+ * Author URI: https://github.com/humanmade
+ *
+ * @package HM/Juicer
+ */
+
+namespace HM\Juicer;
+
+use HM\Autoloader;
+
+Autoloader\register_class_path( __NAMESPACE__, dirname( __FILE__ ) . '/inc/' );
+
+require_once __DIR__ . '/inc/namespace.php';
+
+// Kick it off.
+bootstrap();
