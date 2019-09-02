7626facc8b360f731dd595e718408988fe303ca5
Chris Reynolds <chris@humanmade.com>
2019-09-02 14:27:36 -0600

    add a constant for the juicer api endpoint url

--- plugin.php
+++ plugin.php
@@ -13,6 +13,8 @@ namespace HM\Juicer;
 
 use HM\Autoloader;
 
+const JUICER_ENDPOINT = 'https://www.juicer.io/api/feeds/';
+
 Autoloader\register_class_path( __NAMESPACE__, dirname( __FILE__ ) . '/inc/' );
 
 require_once __DIR__ . '/inc/namespace.php';
