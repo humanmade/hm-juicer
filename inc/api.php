2680ca856abb0bb09b9d6a3a99bfcadbdc497617
Chris Reynolds <chris@humanmade.com>
2019-09-06 14:14:54 -0600

    remove filter in prepare_post_items we have a similar filter in juicer_get_the_content

--- inc/api.php
+++ inc/api.php
@@ -72,7 +72,7 @@ function prepare_post_items( array $items ) : array {
 		$post->post_date           = strtotime( $item->external_created_at );
 		// Translators: %s is a humanized time (e.g. 5 days).
 		$post->post_date_humanized = sprintf( esc_html__( '%s ago', 'hm-juicer' ), human_time_diff( $post->post_date, current_time( 'U' ) ) );
-		$post->post_content        = wp_kses_post( apply_filters( 'juicer_filter_post_content', $item->message ) );
+		$post->post_content        = wp_kses_post( $item->message );
 		$post->image_url           = esc_url_raw( $item->image );
 		$post->additional_images   = $item->additional_photos;
 		$post->source              = esc_html( $item->source->source );
