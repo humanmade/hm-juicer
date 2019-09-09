f74324c2bde1ad30684ee1ac4f06cecbca2963ba
Chris Reynolds <chris@humanmade.com>
2019-09-09 15:03:12 -0600

    test all the helper functions

--- tests/class-test-functions.php
+++ tests/class-test-functions.php
@@ -99,4 +99,135 @@ class Test_Functions extends \WP_UnitTestCase {
 			$juicer_post->id
 		);
 	}
+
+	/**
+	 * Test the get_post function. This just returns the current post object.
+	 */
+	public function test_get_post() {
+		global $juicer_post;
+
+		$this->assertSame(
+			$juicer_post,
+			juicer_get_post()
+		);
+	}
+
+	/**
+	 * Test the get_date function. juicer_get_date can take a parameter for a date format string. By default it returns Unix time.
+	 */
+	public function test_get_date() {
+		$this->assertEquals(
+			strtotime( '2019-09-03T11:50:08.000-07:00' ),
+			juicer_get_date()
+		);
+
+		$this->assertEquals(
+			'2019-09-03 6:50pm',
+			juicer_get_date( 'Y-m-d g:ia' )
+		);
+	}
+
+	/**
+	 * Test the humanized_time function. This uses human_time_diff, and we need that to get the string to test, so this is sort of derivative and not really testing a whole lot.
+	 */
+	public function test_humanized_time() {
+		$humanized_time_diff = human_time_diff( strtotime( '2019-09-03T11:50:08.000-07:00' ), time() );
+
+		$this->assertEquals(
+			sprintf( '%s ago', $humanized_time_diff ),
+			juicer_get_humanized_time()
+		);
+	}
+
+	/**
+	 * Test the get_the_content function.
+	 */
+	public function test_get_the_content() {
+		$content = "<p>Juicer Test is ahead of the curve in the post-acute setting in recognizing patients who are at risk for sepsis with Cerner's Sepsis Management solution. Learn how early intervention prevented our patients from becoming septic and/or transferring from our hospitals 77% of the time. <a target=\"_blank\" class=\"auto\" href=\"https://test.site.dev/2zulhdo\">https://test.site.dev/2zulhdo</a></p>";
+
+		$this->assertEquals(
+			$content,
+			juicer_get_the_content()
+		);
+	}
+
+	/**
+	 * Test get_image_url function.
+	 */
+	public function test_get_image_url() {
+		$this->assertEquals(
+			'https://external.xx.fbcdn.net/safe_image.php?d=AQC_cyqDeqv-mmmZ&w=720&h=720&url=https%3A%2F%2Fblog.testenv.com%2Ftachyon%2Fsites%2F4%2F2019%2F06%2FiStock-1003536156.jpg%3Ffit%3D1254%252C836&cfs=1&sx=0&sy=0&sw=836&sh=836&_nc_hash=AQAN9xUHntdGV7gd',
+			juicer_get_image_url()
+		);
+	}
+
+	/**
+	 * Test the get_source function.
+	 */
+	public function test_get_source() {
+		$this->assertEquals(
+			'Facebook',
+			juicer_get_source()
+		);
+	}
+
+	/**
+	 * Test the get_sharing_link function.
+	 */
+	public function test_get_sharing_link() {
+		$this->assertEquals(
+			'https://test.site.dev/2zulhdo',
+			juicer_get_sharing_link()
+		);
+	}
+
+	/**
+	 * Test the get_like_count function.
+	 */
+	public function test_get_like_count() {
+		$this->assertEquals(
+			47,
+			juicer_get_like_count()
+		);
+	}
+
+	/**
+	 * Test the get_comment_count function.
+	 */
+	public function test_get_comment_count() {
+		$this->assertEquals(
+			1,
+			juicer_get_comment_count()
+		);
+	}
+
+	/**
+	 * Test the get_author_name function.
+	 */
+	public function test_get_author_name() {
+		$this->assertEquals(
+			'Juicer Test',
+			juicer_get_author_name()
+		);
+	}
+
+	/**
+	 * Test the get_author_url function.
+	 */
+	public function test_get_author_url() {
+		$this->assertEquals(
+			'https://facebook.com/profile.php?id=99999999999999999',
+			juicer_get_author_url()
+		);
+	}
+
+	/**
+	 * Test the get_author_image function.
+	 */
+	public function test_get_author_image() {
+		$this->assertEquals(
+			'https://graph.facebook.com/99999999999999999/picture',
+			juicer_get_author_image()
+		);
+	}
 }
