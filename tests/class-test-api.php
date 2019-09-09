4ec64b03c7407d08e2d9e074283e19a638a4f7b4
Chris Reynolds <chris@humanmade.com>
2019-09-09 11:51:20 -0600

    create a helpers functions file for tests move the mock data path and caching helper into helpers

--- tests/class-test-api.php
+++ tests/class-test-api.php
@@ -14,20 +14,12 @@ namespace HM\Juicer;
  */
 class Test_Api extends \WP_UnitTestCase {
 	/**
-	 * Path to Juicer API mock data.
-	 *
-	 * @var string Juicer API mock data path.
+	 * Load the helper functions in the constructor.
 	 */
-	protected static $mock_data = __DIR__ . '/data/juicer-mock.json';
+	public function __construct() {
+		parent::__construct();
 
-	/**
-	 * Cache the mock API data.
-	 *
-	 * We can create a mock API request by simply caching the data in the juicer-mock.json file since Juicer\get_posts checks the cache first.
-	 */
-	private function cache_mock_data() {
-		$api_data = file_get_contents( self::$mock_data );
-		wp_cache_set( 'response_per_10-page_1', $api_data, 'juicer', DAY_IN_SECONDS );
+		require_once __DIR__ . '/helpers.php';
 	}
 
 	/**
@@ -58,7 +50,7 @@ class Test_Api extends \WP_UnitTestCase {
 	 */
 	public function test_get_posts() {
 		// Cache the mock data before trying to get posts.
-		$this->cache_mock_data();
+		\cache_mock_data();
 
 		// Get the posts from Juicer.
 		$posts = get_posts();
