4d1d472600bf3044c1158af6bcc0252c6c4fb583
Chris Reynolds <chris@humanmade.com>
2019-09-09 12:37:05 -0600

    set up the juicer query before every test

--- tests/class-test-functions.php
+++ tests/class-test-functions.php
@@ -5,6 +5,8 @@
  * @package HM\Juicer
  */
 
+namespace HM\Juicer;
+
 /**
  * The main test class
  */
@@ -16,6 +18,22 @@ class Test_Functions extends \WP_UnitTestCase {
 		parent::__construct();
 
 		require_once __DIR__ . '/helpers.php';
+	}
+
+	/**
+	 * Before each test.
+	 * We want to set up the $juicer_posts global before each test because we use it in all tests.
+	 */
+	public function setUp() {
+		global $juicer_posts;
+
+		parent::setUp();
+
+		// Cache the mock data so we can use it.
+		cache_mock_data();
+
+		// Set up $juicer_posts global. Usually this happens in the juicer_feed function.
+		$juicer_posts = get_posts();
 	}
 
 }
