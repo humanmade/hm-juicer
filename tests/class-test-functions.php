1b16d7e8134fb41411ff5f0e3ad0266fd0082e3c
Chris Reynolds <chris@humanmade.com>
2019-09-09 12:36:43 -0600

    require the helper functions

--- tests/class-test-functions.php
+++ tests/class-test-functions.php
@@ -9,4 +9,13 @@
  * The main test class
  */
 class Test_Functions extends \WP_UnitTestCase {
+	/**
+	 * Load the helper functions in the constructor.
+	 */
+	public function __construct() {
+		parent::__construct();
+
+		require_once __DIR__ . '/helpers.php';
+	}
+
 }
