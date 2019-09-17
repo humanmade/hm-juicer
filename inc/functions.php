fcc847114cf7d66bbe972344bc6d4645f9188a88
Chris Reynolds <chris@humanmade.com>
2019-09-17 15:39:02 -0600

    fix load more doc comment

--- inc/functions.php
+++ inc/functions.php
@@ -473,7 +473,7 @@ function juicer_the_author_image() {
  * @param string $template  (Required) The template to load (e.g. 'feed' or
  * 'post'), not including the prefix ('part-juicer').
  * @param int    $count     The number of posts to load.
- * @param bool   $load_more Should we display a load more button?
+ * @param bool   $load_more Whether to display a load more button.
  */
 function juicer_get_template( string $template, int $count = 10, bool $load_more = false ) {
 	/**
