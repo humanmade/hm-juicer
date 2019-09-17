75d6b99338a766cdadfdeb6a04e94db213d81274
Chris Reynolds <chris@humanmade.com>
2019-09-17 16:23:59 -0600

    bootstrap it up

--- plugin.php
+++ plugin.php
@@ -11,6 +11,8 @@
 
 namespace HM\Juicer;
 
+use HM\Juicer\LoadMore;
+
 const JUICER_ENDPOINT = 'https://www.juicer.io/api/feeds/';
 
 require_once __DIR__ . '/inc/namespace.php';
@@ -20,3 +22,4 @@ require_once __DIR__ . '/inc/load-more.php';
 
 // Kick it off.
 bootstrap();
+LoadMore\bootstrap();
