b095bffa1ee2a0987b1b38788467cce9d4667dc8
Chris Reynolds <chris@humanmade.com>
2019-10-29 10:50:12 -0600

    rename .webpack to .config

---
+++ .config/webpack-config-prod.js
@@ -0,0 +1,17 @@
+/**
+ * .config/webpack.config.prod.js
+ * This file defines the production build configuration
+ */
+const { helpers, externals, presets } = require( '@humanmade/webpack-helpers' );
+const { filePath } = helpers;
+
+module.exports = presets.production( {
+	externals,
+	entry: {
+		juicer: filePath( 'assets/js/juicer.js' ),
+		load_more: filePath( 'assets/js/load-more.js' ),
+	},
+	output: {
+		path: filePath( 'build' ),
+	},
+ } );
