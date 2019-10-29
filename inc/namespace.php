470187a30a8eb271f5a792e9d701eb013cf78bfd
Chris Reynolds <chris@humanmade.com>
2019-10-29 16:22:54 -0600

    remove whitespace

--- inc/namespace.php
+++ inc/namespace.php
@@ -18,12 +18,12 @@ const JUICER_ENDPOINT = 'https://www.juicer.io/api/feeds/';
  * Kick everything off.
  */
 function bootstrap() {
-	if ( 
+	if (
 		// If none of the Juicer constants are defined...
 		! defined( 'JUICER_ID' ) ||
 		! defined( 'JUICER_SHORT_URL' ) ||
 		! defined( 'JUICER_LONG_URL' ) ||
-		! defined( 'JUICER_SITE_NAME' ) 
+		! defined( 'JUICER_SITE_NAME' )
 	) {
 		// ...load the settings page.
 		require_once __DIR__ . '/settings.php';
